70cc6f72f62b6dd41a7391f608a50300aa8557a1
sb171732 <sauronkan@gmail.com>
2023-10-30 17:23:22 +0700

    1st init

---
+++ src/components/UslovView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="">
+        <h1 v-if="seen == true">Видно?   </h1>
+        <h1 v-else> Не видно  </h1>
+        
+        <h2 v-if="col<3"> {{ m }}  </h2>
+
+        <h3 v-else> {{ col-5 }}  </h3>
+    </div>
+</template>
+<script>
+   export default {
+    data(){
+        return{
+            message:'Видно ',
+            m:'2',
+            col:6,
+            seen:true
+        }
+    }
+   }
+
+</script>
