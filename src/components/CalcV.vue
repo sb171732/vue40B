cf09d89776c5c2ae927a0a65e377dd8a1ffc4290
sb171732 <sauronkan@gmail.com>
2023-11-03 12:28:04 +0700

    calc computed

--- src/components/CalcV.vue
+++ src/components/CalcV.vue
@@ -3,8 +3,14 @@
        <p> x <input v-model="x"> </p> 
        <p> y <input v-model="y"></p>
           <hr>        
-        = <span v-text="result"></span>
-</div> 
+       Сложение = <span v-text="plus" ></span> 
+       <hr>
+       Вычитание = <span v-text="minus" ></span>
+       <hr>
+       Умножение = <span v-text="umn" ></span>
+       <hr>
+       Деление = <span v-text="del" ></span>
+    </div> 
 </template>
 <script> 
     function isNotNumericValue(value) { 
@@ -19,17 +25,27 @@
             lastResult: 0 }
     }, 
     computed:{
-           result(){
-            
-            if(isNotNumericValue(parseFloat(this.x)))
-                return this.lastResult;
-
-            
-            if(isNotNumericValue(parseFloat(this.y))) 
-                return this.lastResult; 
-            
-            return parseFloat(this.x) + parseFloat(this.y)
-           }
+           plus(){
+                if(isNotNumericValue(parseFloat(this.x))) return this.lastResult;
+                if(isNotNumericValue(parseFloat(this.y))) return this.lastResult; 
+                return parseFloat(this.x) + parseFloat(this.y)
+           },
+           minus(){
+                if(isNotNumericValue(parseFloat(this.x))) return this.lastResult;
+                if(isNotNumericValue(parseFloat(this.y))) return this.lastResult; 
+                return parseFloat(this.x) - parseFloat(this.y)
+           },
+           umn(){
+                if(isNotNumericValue(parseFloat(this.x))) return this.lastResult;
+                if(isNotNumericValue(parseFloat(this.y))) return this.lastResult; 
+                return parseFloat(this.x) * parseFloat(this.y)
+           },
+           del(){
+                if(isNotNumericValue(parseFloat(this.x))) return this.lastResult;
+                if(isNotNumericValue(parseFloat(this.y))) return this.lastResult; 
+                if (this.y==0) return "Деление на ноль"
+                else return parseFloat(this.x) / parseFloat(this.y)
+           },
     }
 }
 </script>
